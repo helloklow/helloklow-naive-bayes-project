2b3794066b78d58f5adbc7c50da676f1b0aef106
George Perdrizet <george@perdrizet.org>
2024-11-08 17:09:34 -0500

    Finished MVP

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 import pandas as pd
+from sklearn.model_selection import cross_val_score, StratifiedKFold
 
 # load the .env file variables
 load_dotenv()
@@ -11,3 +12,28 @@ def db_connect():
     engine = create_engine(os.getenv('DATABASE_URL'))
     engine.connect()
     return engine
+
+
+def cross_val(model, features: pd.DataFrame, labels: pd.Series) -> list[float]:
+    '''Reusable helper function to run cross-validation on a model. Takes model,
+    Pandas data frame of features and Pandas data series of labels. Returns 
+    list of cross-validation fold accuracy scores as percents.'''
+
+    # Define the cross-validation strategy
+    cross_validation=StratifiedKFold(n_splits=7, shuffle=True, random_state=315)
+
+    # Run the cross-validation, collecting the scores
+    scores=cross_val_score(
+        model,
+        features,
+        labels,
+        cv=cross_validation,
+        n_jobs=-1,
+        scoring='accuracy'
+    )
+
+    # Print mean and standard deviation of the scores
+    print(f'Cross-validation accuracy: {(scores.mean() * 100):.2f} +/- {(scores.std() * 100):.2f}%')
+
+    # Return the scores
+    return scores
